10316c4a4cc5b870df695a9320b015bc2aaf06bc
Matthew DeCarlo <mdecarlo0917@gmail.com>
2020-02-20 08:41:36 -0700

    Starting code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { PassListComponent } from './passes/pass-list.component';
 import { VehicleListComponent } from './vehicles/vehicle-list.component';
 import { VehicleDetailComponent } from './vehicles/vehicle-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { VehicleDetailGuard } from './vehicles/vehicle-detail.guard';
 
 @NgModule({
   declarations: [
@@ -18,15 +16,7 @@ import { VehicleDetailGuard } from './vehicles/vehicle-detail.guard';
     WelcomeComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-      { path: 'passes', component: PassListComponent },
-      { path: 'vehicles', component: VehicleListComponent },
-      { path: 'vehicles/:id', component: VehicleDetailComponent, canActivate: [VehicleDetailGuard] },
-      { path: "welcome", component: WelcomeComponent },
-      { path: "", redirectTo: "welcome", pathMatch: "full" },
-      { path: "**", redirectTo: "welcome", pathMatch: "full" }
-    ])
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
